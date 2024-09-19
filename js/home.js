9ae872c0e8434776ce92f5130a448cde5a2b6f8a
Jehadi1 <mdjehadihasan75@gmail.com>
2024-09-19 14:50:31 +0600

    add this code

--- js/home.js
+++ js/home.js
@@ -1,9 +1,29 @@
 document.getElementById('add-money-btn')
         .addEventListener('click', function(event){
             event.preventDefault();
-            // console.log('added money')
+
+
             const addAmount = document.getElementById('add-amount').value
-            console.log(addAmount)
+            console.log(typeof addAmount); 
+
             const addPin = document.getElementById('add-pin').value
-            console.log(addPin)
+            console.log(typeof addPin);
+
+
+            if(addPin === '1234'){
+                console.log(typeof'added Money')
+
+                const balance = document.getElementById('current-amount').innerText
+                console.log(balance)
+
+                const addMoneyNumber = parseFloat(addAmount);
+                const balanceNumber  = parseFloat(balance);
+                const upadateBalance = addMoneyNumber + balanceNumber;
+                console.log(upadateBalance);
+
+                document.getElementById('current-amount').innerText = upadateBalance;
+            }
+            else{
+                alert('Worng Pin')
+            }
         })
